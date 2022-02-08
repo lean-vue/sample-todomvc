f597074322df62362aa08765d39ab6f684dda0bb
Micha Alt <mail@lean-stack.de>
2022-02-08 22:34:24 +0100

    Install and configure linting setup.

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+    'vue/setup-compiler-macros': true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {},
+  overrides: [
+    {
+      files: ['cypress/integration/**.spec.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
+    },
+  ],
+};
