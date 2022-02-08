9630024608cde27f629c22ec1327c2751b9fcf4d
Micha Alt <mail@lean-stack.de>
2022-02-08 21:13:36 +0100

    Implement minimal Vue app.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>todos</h1>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+h1 {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  font-weight: 400;
+  font-size: 6rem;
+  margin: 4rem auto 0;
+  max-width: 420px;
+  text-align: center;
+}
+</style>
