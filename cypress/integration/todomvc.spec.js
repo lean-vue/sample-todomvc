8f6848d224fc2772dc089f7bdcbcb74428100741
Micha Alt <mail@lean-stack.de>
2022-02-08 22:13:40 +0100

    Install and configure Cypress for E2E testing.

---
+++ cypress/integration/todomvc.spec.js
@@ -0,0 +1,29 @@
+describe('Vue TodoMVC', () => {
+  const selectors = {
+    main: '.main',
+    footer: '.footer',
+    newTodo: '.new-todo',
+    todoItems: '.todo-list li',
+  };
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  context('When page is initially opened', () => {
+    it('should focus on the todo input field', () => {
+      cy.focused().should('have.class', selectors.newTodo.slice(1));
+    });
+  });
+
+  context('No Todos', function () {
+    it('starts with nothing', () => {
+      cy.get(selectors.todoItems).should('have.length', 0);
+    });
+
+    it('should hide main section and footer toolbar', function () {
+      cy.get(selectors.main).should('not.be.visible');
+      cy.get(selectors.footer).should('not.be.visible');
+    });
+  });
+});
