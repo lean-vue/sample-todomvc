9764841644e9ecfa2d6d5784dde659cc1b004ba8
Micha Alt <mail@lean-stack.de>
2022-02-09 23:21:08 +0100

    Implement special editing aspects.

--- src/components/TodosItem.vue
+++ src/components/TodosItem.vue
@@ -1,17 +1,25 @@
 <template>
   <li :class="{ completed: todo.completed, editing: editMode }">
-    <div class="view">
+    <div v-if="!editMode" class="view">
       <input class="toggle" type="checkbox" :checked="todo.completed" @change="toggleTodo(todo)" />
       <label @dblclick="beginEdit">{{ todo.title }}</label>
       <button class="destroy" @click="destroyTodo(todo.id)"></button>
     </div>
-    <input v-model="editTitle" class="edit" @keyup.enter="commitEdit" @blur="commitEdit" />
+    <input
+      v-else
+      ref="editElt"
+      v-model.trim="editTitle"
+      class="edit"
+      @keyup.enter="commitEdit"
+      @blur="commitEdit"
+      @keyup.esc="rollbackEdit"
+    />
   </li>
 </template>
 
 <script setup>
 import useAppState from '@/state/app-state';
-import { ref } from 'vue';
+import { nextTick, ref } from 'vue';
 
 const props = defineProps({
   todo: {
@@ -25,14 +33,27 @@ const { toggleTodo, updateTodoTitle, destroyTodo } = useAppState();
 // Editing Feature
 const editMode = ref(false);
 const editTitle = ref(props.todo.title);
+const editElt = ref(null);
 
-const beginEdit = () => editMode.value = true;
+const beginEdit = () => {
+  editMode.value = true; nextTick(() => {
+    editElt.value.focus();
+  })
+}
+
 const commitEdit = () => {
+  if (!editMode.value) return;
+
   if (editTitle.value.length > 0) {
     updateTodoTitle(props.todo.id, editTitle.value);
   } else {
     destroyTodo(props.todo.id);
   }
+  editMode.value = false;
+}
+
+const rollbackEdit = () => {
+  editTitle.value = props.todo.title;
   editMode.value = false;
 }
 </script>
