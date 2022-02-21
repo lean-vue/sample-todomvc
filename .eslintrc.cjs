593b3b373b2afbe2a59ca8ce4a5ccdff58262273
Micha Alt <mail@lean-stack.de>
2022-02-21 23:02:51 +0100

    Make eslint typescript aware.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,15 @@ module.exports = {
     es2021: true,
     'vue/setup-compiler-macros': true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
