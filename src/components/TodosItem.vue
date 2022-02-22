decd2ca522fa681daab4bbfc5d5a1e4a4e0d5244
Micha Alt <mail@lean-stack.de>
2022-02-22 22:49:15 +0100

    Display correct completed state.

--- src/components/TodosItem.vue
+++ src/components/TodosItem.vue
@@ -5,10 +5,10 @@ defineProps<{ todo: Todo }>();
 </script>
 
 <template>
-  <!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
-  <li class="completed">
+  <!-- List items should get the class `editing` when editing -->
+  <li :class="{ completed: todo.completed }">
     <div class="view">
-      <input class="toggle" type="checkbox" checked />
+      <input class="toggle" type="checkbox" :checked="todo.completed" />
       <label>{{ todo.title }}</label>
       <button class="destroy"></button>
     </div>
