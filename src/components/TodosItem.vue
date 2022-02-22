3b21770996a3c604cc3f73e7ccaf3c8a82ae498f
Micha Alt <mail@lean-stack.de>
2022-02-22 23:03:33 +0100

    Handle toggle state in todos-item.

--- src/components/TodosItem.vue
+++ src/components/TodosItem.vue
@@ -1,14 +1,22 @@
 <script lang="ts" setup>
 import Todo from '@/model/todo';
+import useAppStore from '@/store/use-app-store';
 
 defineProps<{ todo: Todo }>();
+
+const { toggleTodo } = useAppStore();
 </script>
 
 <template>
   <!-- List items should get the class `editing` when editing -->
   <li :class="{ completed: todo.completed }">
     <div class="view">
-      <input class="toggle" type="checkbox" :checked="todo.completed" />
+      <input
+        class="toggle"
+        type="checkbox"
+        :checked="todo.completed"
+        @change="toggleTodo(todo)"
+      />
       <label>{{ todo.title }}</label>
       <button class="destroy"></button>
     </div>
