330b3150477a8d84a8e4909ca09e629b0a88d8ea
Micha Alt <mail@lean-stack.de>
2022-02-22 22:39:20 +0100

    Render all todo items (filtered).

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup>
 import TodosItem from '@/components/TodosItem.vue';
+import useAppStore from '@/store/use-app-store';
+
+const { filteredTodos } = useAppStore();
 </script>
 
 <template>
   <ul class="todo-list">
-    <TodosItem />
+    <TodosItem v-for="todo in filteredTodos" :key="todo.id" />
   </ul>
 </template>
