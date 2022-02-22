bf0a4b6ad1b7fea2fddaeb4647ae648ee7acac25
Micha Alt <mail@lean-stack.de>
2022-02-22 22:26:46 +0100

    Implement create todo in store.

--- src/store/use-app-store.ts
+++ src/store/use-app-store.ts
@@ -1,15 +1,26 @@
 import { computed, ref } from 'vue';
+import LocalPersistence from '@/api/local-persistence';
 import Todo from '@/model/todo';
+
+const persistence = new LocalPersistence();
 
 const state = {
   todos: ref<Todo[]>([]),
   visibility: ref<'all' | 'active' | 'completed'>('all'),
 };
 
+// actions
+const createTodo = async (title: string) => {
+  const todo = await persistence.create(title);
+  state.todos.value = [...state.todos.value, todo];
+};
+
 // computed getters
 const hasTodos = computed(() => state.todos.value.length > 0);
 
 const useAppStore = () => ({
+  // actions
+  createTodo,
   // getters
   hasTodos,
 });
